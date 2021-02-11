b2d5694593a0d1ecd77276b2e33e583add8ed216
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-02-11 13:35:22 -0600

    Added logos. css and header

--- breaking_bad_api/src/App.js
+++ breaking_bad_api/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/ui/Header'
 
 const App = () => {
   return (
     <div className="container">
-      HELLO
+      <Header />
     </div>
   );
 }
