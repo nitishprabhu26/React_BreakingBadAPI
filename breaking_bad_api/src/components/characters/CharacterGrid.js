a5124714855326e1de04e052a464ccde3867bca1
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-02-20 00:56:15 -0600

    Added spinner

--- breaking_bad_api/src/components/characters/CharacterGrid.js
+++ breaking_bad_api/src/components/characters/CharacterGrid.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
 
 
 const CharacterGrid = ({ items, isLoading }) => {
     return isLoading ? (
-      <h1>Loading...</h1>
+        <Spinner />
     ) : (
       <section className='cards'>
         {items.map((item) => (
