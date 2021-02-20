761827beab012ee54a99495ff68fe83b23448279
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-02-19 23:27:07 -0600

    made a http request using axios

--- breaking_bad_api/src/App.js
+++ breaking_bad_api/src/App.js
@@ -1,7 +1,25 @@
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Header from './components/ui/Header'
+// axios returns a promise
+import axios from 'axios'
 
 const App = () => {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
       <Header />
